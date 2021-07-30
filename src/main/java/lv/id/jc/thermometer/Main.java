0b1a548d8f8c6d83f07e2815680050cc4855a177
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-07-30 18:52:07 +0300

    Refactor Main class.

--- src/main/java/lv/id/jc/thermometer/Main.java
+++ src/main/java/lv/id/jc/thermometer/Main.java
@@ -4,10 +4,13 @@ public class Main {
     public static void main(String[] args) {
         final var thermometer = Thermometer.of(23.19);
 
+        System.out.println(thermometer);
+        System.out.println();
+
         System.out
-                .printf("Thermometer: %20s%n%n", thermometer)
                 .printf("Scale: %f%n", thermometer.getScale())
                 .printf("State: %s%n", thermometer.getState())
+                .printf("Thermometer: %s%n", thermometer)
                 .printf("Thermometer: %4.1f° (%s)%n", thermometer.getScale(), thermometer.getState());
     }
 }
